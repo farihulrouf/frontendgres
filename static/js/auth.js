0daf621eb975480740f8367b5bf334113d44d8e1
Farihul Rouf <farihulrouf@gmail.com>
2023-10-19 21:02:03 +0700

    inix x

---
+++ static/js/auth.js
@@ -0,0 +1,29 @@
+const token = localStorage.getItem('jwt');
+//console.log("data token",token)
+if(token==null){
+    window.location.href = './index.html'
+}
+else {
+    const jwtPayload = JSON.parse(window.atob(token.split('.')[1]))
+    const isExpired = Date.now() >= jwtPayload.exp * 1000;
+    if(isExpired){
+        window.location.href = './index.html'
+    }
+    else {
+        const user = localStorage.getItem('user');
+        const data = JSON.parse(user)
+        console.log(data.name)
+        str_body = ""
+        str_body += `
+        ${data.name} 
+        `
+        $("#namauser").html(str_body)
+        //console.log(JSON.parse(user))
+    }
+}
+/*
+  var jwt = localStorage.getItem("jwt");
+  if (jwt != null) {
+      window.location.href = './datamaster.html'
+  }
+  */
